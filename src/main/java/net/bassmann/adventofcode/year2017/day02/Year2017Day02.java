4dcfebc45e2cee8ff9217a95dcba11dbf75f849a
Felix Rabe <felix.rabe@gmail.com>
2017-12-01 07:07:34 +0100

    Prepare 2017 day 2

---
+++ src/main/java/net/bassmann/adventofcode/year2017/day02/Year2017Day02.java
@@ -0,0 +1,20 @@
+package net.bassmann.adventofcode.year2017.day02;
+
+import net.bassmann.adventofcode.common.AbstractDay;
+
+public class Year2017Day02 extends AbstractDay {
+
+  public Year2017Day02() {
+    super(2017, 2);
+  }
+
+  @Override
+  public String solvePart1() {
+    return null;
+  }
+
+  @Override
+  public String solvePart2() {
+    return null;
+  }
+}
---
+++ src/test/java/net/bassmann/adventofcode/year2017/day02/Year2017Day02Test.java
@@ -0,0 +1,21 @@
+package net.bassmann.adventofcode.year2017.day02;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import net.bassmann.adventofcode.common.Day;
+import org.junit.jupiter.api.Test;
+
+class Year2017Day02Test {
+
+  private final Day today = new Year2017Day02();
+
+  @Test
+  void solvePart1() {
+    assertEquals(null, today.solvePart1());
+  }
+
+  @Test
+  void solvePart2() {
+    assertEquals(null, today.solvePart2());
+  }
+}
