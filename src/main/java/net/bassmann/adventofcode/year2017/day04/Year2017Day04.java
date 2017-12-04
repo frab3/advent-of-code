9472776c147118d28a871f61ee8c26696d0fdded
Felix Rabe <felix.rabe@gmail.com>
2017-12-04 06:31:07 +0100

    Add 2017 day 4 solution

--- src/main/java/net/bassmann/adventofcode/year2017/day04/Year2017Day04.java
+++ src/main/java/net/bassmann/adventofcode/year2017/day04/Year2017Day04.java
@@ -1,7 +1,58 @@
 package net.bassmann.adventofcode.year2017.day04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import net.bassmann.adventofcode.common.AbstractDay;
 
+/**
+ * Day 4: High-Entropy Passphrases.
+ *
+ * <p>A new system policy has been put in place that requires all accounts to use a passphrase
+ * instead of simply a password. A passphrase consists of a series of words (lowercase letters)
+ * separated by spaces.
+ *
+ * <p>To ensure security, a valid passphrase must contain no duplicate words.
+ *
+ * <p>For example:
+ *
+ * <ul>
+ * <li>aa bb cc dd ee is valid.
+ * <li>aa bb cc dd aa is not valid - the word aa appears more than once.
+ * <li>aa bb cc dd aaa is valid - aa and aaa count as different words.
+ * </ul>
+ *
+ * The system's full passphrase list is available as your puzzle input.
+ *
+ * <h2>Part One</h2>
+ *
+ * How many passphrases are valid?
+ *
+ * <h2>Part Two</h2>
+ *
+ * For added security, yet another system policy has been put in place. Now, a valid passphrase must
+ * contain no two words that are anagrams of each other - that is, a passphrase is invalid if any
+ * word's letters can be rearranged to form any other word in the passphrase.
+ *
+ * <p>For example:
+ *
+ * <ul>
+ * <li>abcde fghij is a valid passphrase.
+ * <li>abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form
+ * the first word.
+ * <li>a ab abc abd abf abj is a valid passphrase, because all letters need to be used when
+ * forming another word.
+ * <li>iiii oiii ooii oooi oooo is valid.
+ * <li>oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other
+ * word.
+ * </ul>
+ *
+ * Under this new system policy, how many passphrases are valid?
+ */
 public class Year2017Day04 extends AbstractDay {
 
   public Year2017Day04() {
@@ -10,11 +61,31 @@ public class Year2017Day04 extends AbstractDay {
 
   @Override
   public String solvePart1() {
-    return null;
+    List<String> phrases = getRiddleInput().asList();
+    long count = phrases.stream().filter(Year2017Day04::isValid).count();
+    return Long.toString(count);
   }
 
   @Override
   public String solvePart2() {
-    return null;
+    List<String> phrases = getRiddleInput().asList();
+    long count = phrases.stream().filter(Year2017Day04::isValidNoAnagrams).count();
+    return Long.toString(count);
+  }
+
+  static boolean isValid(String input) {
+    String[] split = input.split(" ");
+    Set<String> set = new HashSet<>(Arrays.asList(split));
+    return set.size() == split.length;
+  }
+
+  static boolean isValidNoAnagrams(String input) {
+    List<String> list = new ArrayList<>();
+    for (String s : input.split(" ")) {
+      String t = Stream.of(s.split("")).sorted().collect(Collectors.joining());
+      list.add(t);
+    }
+    Set<String> set = new HashSet<>(list);
+    return set.size() == list.size();
   }
 }
--- src/test/java/net/bassmann/adventofcode/year2017/day04/Year2017Day04Test.java
+++ src/test/java/net/bassmann/adventofcode/year2017/day04/Year2017Day04Test.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import net.bassmann.adventofcode.common.Day;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class Year2017Day04Test {
 
@@ -11,11 +13,29 @@ class Year2017Day04Test {
 
   @Test
   void solvePart1() {
-    assertEquals(null, today.solvePart1());
+    assertEquals("466", today.solvePart1());
   }
 
   @Test
   void solvePart2() {
-    assertEquals(null, today.solvePart2());
+    assertEquals("251", today.solvePart2());
+  }
+
+  @ParameterizedTest
+  @CsvSource({"aa bb cc dd ee, true", "aa bb cc dd aa, false", "aa bb cc dd aaa, true"})
+  void isValidTest(String input, boolean expected) {
+    assertEquals(expected, Year2017Day04.isValid(input));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "abcde fghij, true",
+      "abcde xyz ecdab, false",
+      "a ab abc abd abf abj, true",
+      "iiii oiii ooii oooi oooo, true",
+      "oiii ioii iioi iiio, false"
+  })
+  void isValidNoAnagramsTest(String input, boolean expected) {
+    assertEquals(expected, Year2017Day04.isValidNoAnagrams(input));
   }
 }
